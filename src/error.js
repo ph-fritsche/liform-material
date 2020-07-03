38a468fe203b363549a4c53de51e30aeee905c16
Philipp Fritsche <ph.fritsche@gmail.com>
2020-07-03 19:21:26 +0200

    Fix field validation errors for nested fields

--- src/error.js
+++ src/error.js
@@ -3,8 +3,8 @@ import { liformizeName } from 'liform-react-final/dist/field'
 export const getFieldError = (liform, finalName, meta) => {
     const liformName = liformizeName(finalName)
     return (
-        (meta.touched || meta.dirty) && meta.error
-            && meta.error.join('\n')
+        (meta.touched || meta.dirty) && liform.validationErrors && liform.validationErrors[liformName]
+            && liform.validationErrors[liformName].join('\n')
         || meta.pristine && liform.meta.errors && liform.meta.errors[liformName]
             && liform.meta.errors[liformName].join('\n')
     )
