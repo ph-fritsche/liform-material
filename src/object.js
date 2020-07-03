d0d0a9c9e3186e38d9e6b5051f3808f30c5b660b
Philipp Fritsche <ph.fritsche@gmail.com>
2020-07-03 19:23:11 +0200

    Use Grid for object layout #6

--- src/object.js
+++ src/object.js
@@ -1,39 +1,46 @@
 import React from 'react'
 import { Field } from 'react-final-form'
-import { FormGroup, FormControl, FormLabel, FormHelperText } from '@material-ui/core'
+import { FormLabel, FormHelperText, Grid } from '@material-ui/core'
 import Lifield, { finalizeName } from 'liform-react-final/dist/field'
 import { mapProperties } from 'liform-react-final/dist/properties'
 
 import { getFieldError } from './error'
 
 export const ObjectWidget = ({liform, name, schema, ...props}) => (
-    <Field
-        name={finalizeName(name)}
-        subscription="error"
-        render={({meta}) => {
-            const error = getFieldError(liform, finalizeName(name), meta)
-            return (
-                <FormControl
-                    component='fieldset'
-                    error={!!error}
-                >
-                    <FormLabel component='legend'>{schema.title}</FormLabel>
-                    <FormHelperText>{error || schema.description}</FormHelperText>
-                    <FormGroup>
-                        { mapProperties(schema.properties || {}, (propSchema, key) => (
-                            <Lifield key={key}
-                                {...props}
-                                liform={liform}
-                                name={ (name || '') + ((name && String(name).slice(-1) !== ']') ? '.' : '') + key }
-                                schema={ propSchema }
-                                required={ Array.isArray(schema.required) && schema.required.indexOf(key) >= 0 }
-                            />
-                        )) }
-                    </FormGroup>
-                </FormControl>
-            )
-        }}
-    />
+    <Grid container
+        spacing={ schema.attr.gridSpacing || (name ? 1 : 3) }
+    >
+        <Field
+            name={finalizeName(name)}
+            render={({meta}) => {
+                const error = getFieldError(liform, finalizeName(name), meta)
+
+                return (error || schema.title) && (
+                    <Grid item xs={12}>
+                        <div
+                            style={{marginTop: (schema.attr.gridSpacing || (name ? 1 : 3)) * .5 - .5 + 'em' }}
+                        >
+                            <FormLabel component='legend' error={!!error}>{schema.title}</FormLabel>
+                            <FormHelperText error={!!error}>{error || schema.description}</FormHelperText>
+                        </div>
+                    </Grid>
+                )
+            }}
+        />
+        { mapProperties(schema.properties || {}, (propSchema, key) => console.log(propSchema.attr.gridSize) || (
+            <Grid item key={key}
+                { ...(typeof(propSchema.attr.gridSize) === 'object' ? propSchema.attr.gridSize : {xs: propSchema.attr.gridSize}) }
+            >
+                <Lifield key={key}
+                    {...props}
+                    liform={liform}
+                    name={ (name || '') + ((name && String(name).slice(-1) !== ']') ? '.' : '') + key }
+                    schema={ propSchema }
+                    required={ Array.isArray(schema.required) && schema.required.indexOf(key) >= 0 }
+                />
+            </Grid>
+        )) }
+    </Grid>
 )
 
 export default ObjectWidget
