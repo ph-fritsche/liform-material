6cb83b8d391318702317fd471377966c42ca7580
Philipp Fritsche <ph.fritsche@gmail.com>
2020-07-06 19:11:09 +0200

    Extract AspectInput from DateTimeInput

---
+++ src/AspectInput/AspectInput.js
@@ -0,0 +1,234 @@
+import React, { useEffect, useReducer, useRef, useState } from 'react'
+import { makeStyles } from '@material-ui/core'
+import clsx from 'clsx'
+import { useForkedRef } from '../util/ref'
+import { shallowEqual } from '../util/equal'
+
+const useStyle = makeStyles(theme => ({
+    value: {
+    },
+    valueFocus: {
+        background: theme.palette.type === 'light' ? theme.palette.primary.light : theme.palette.primary.dark,
+        color: theme.palette.primary.contrastText,
+    },
+}))
+
+const resetInput = aspects => {
+    const i = aspects.findIndex(p => p.type === 'value')
+    const v = aspects[i].value
+    return {index: i, value: v}
+}
+
+export const AspectInput = React.forwardRef(function AspectInput(props, ref) {
+    const {
+        className,
+        classes,
+
+        onBlur: onBlurProp,
+        onFocus: onFocusProp,
+
+        // callback receiving the partial value and the index of the aspect
+        // should return the (corrected) value or undefined for invalid inputs
+        validate: validateProp,
+
+        // callback receiving the partial value and the index of the aspect
+        // should update the component value
+        commit: commitProp,
+
+        // should be an array of objects like {type:'value'|'placeholder', value?: any, placeholder: string}
+        aspects,
+        display,
+        placeholder,
+
+        ...others
+    } = props
+
+    const style = useStyle()
+
+    const inputRef = useRef()
+    const forkedInputRef = useForkedRef(ref, inputRef)
+
+    const [isInputFocused, setInputFocus] = useState(false)
+
+    const [activeAspect, updateAspect] = useReducer((state, action) => {
+        if (action.type === 'moveFocus' && action.step) {
+            for (let i = state.index + action.step; i >= 0 && i < aspects.length; i += action.step) {
+                if (aspects[i].type === 'value') {
+                    return {index: i, value: aspects[i].value}
+                }
+            }
+        }
+        if (action.type === 'setFocus' && action.index !== undefined) {
+            return {index: action.index, value: aspects[action.index].value}
+        }
+        if (action.type === 'reset') {
+            return resetInput(aspects)
+        }
+        if (action.type === 'change') {
+            return {index: state.index, value: action.value}
+        }
+        return state
+    }, aspects, resetInput)
+
+    useEffect(() => {
+        if (isInputFocused && aspects[activeAspect.index].value != activeAspect.value) {
+            updateAspect({type: 'change', value: aspects[activeAspect.index].value})
+        } else if (!isInputFocused && !shallowEqual(resetInput(aspects), activeAspect)) {
+            updateAspect({type: 'reset'})
+        }
+    }, [aspects])
+
+    const commitAspect = committedValue => {
+        const newValue = validateProp(committedValue, activeAspect.index)
+        if (newValue === undefined) {
+            return
+        }
+
+        commitProp(committedValue, activeAspect.index)
+    }
+
+    const onChange = event => {
+        let newValue = event.target.value === ''
+            || validateProp(event.target.value, activeAspect.index)
+            ? event.target.value
+            : validateProp(event.target.value.substr(-1), activeAspect.index)
+                && event.target.value.substr(-1)
+
+        if (newValue === undefined) {
+            return
+        }
+
+        const isNumericInput = (aspects[activeAspect.index].isNumeric ?? 'numeric') === 'numeric'
+
+        const paddedValue = isNumericInput
+            ? String(Number(newValue)).padStart(aspects[activeAspect.index].placeholder.length, '0')
+            : String(newValue).padStart(aspects[activeAspect.index].placeholder.length, ' ')
+
+        const isEmpty = isNumericInput ? newValue == 0 : String(newValue).trim() == ''
+
+        // determine if there can be another char in this field without overflow
+        const nextCharOverflow = !isEmpty && (
+            isNumericInput
+                ? validateProp(newValue + '0', activeAspect.index) === undefined
+                : String(newValue).trim().length >= aspects[activeAspect.index].placeholder.length
+        )
+
+        if (nextCharOverflow) {
+            updateAspect({type: 'change', value: paddedValue})
+            updateAspect({type: 'moveFocus', step: 1})
+            commitAspect(paddedValue)
+        } else {
+            updateAspect({type: 'change', value: paddedValue})
+        }
+    }
+
+    const onKeyDown = event => {
+        const moveKeys = {
+            'ArrowLeft': -1,
+            'ArrowRight': 1,
+        }
+        if (moveKeys[event.key]) {
+            event.preventDefault()
+
+            commitAspect(event.target.value)
+            updateAspect({type: 'moveFocus', step: moveKeys[event.key]})
+        }
+
+        const addKeys = {
+            'ArrowDown': -1,
+            'ArrowUp': 1,
+        }
+        if (addKeys[event.key]) {
+            event.preventDefault()
+
+            const isNumericInput = (aspects[activeAspect.index].isNumeric ?? 'numeric') === 'numeric'
+
+            let newValue = isNumericInput
+                ? Number(event.target.value) + addKeys[event.key]
+                : String(event.target.value).length > 1 && String.fromCharCode(String(event.target.value).charCodeAt(-1) + addKeys[event.key])
+
+            newValue = validateProp(newValue, activeAspect.index)
+
+            if (newValue === undefined) {
+                return
+            }
+
+            const paddedValue = isNumericInput
+                ? String(Number(newValue)).padStart(aspects[activeAspect.index].placeholder.length, '0')
+                : String(newValue).padStart(aspects[activeAspect.index].placeholder.length, ' ')
+
+            updateAspect({type: 'change', value: paddedValue})
+        }
+    }
+
+    const onFocus = () => setInputFocus(true)
+
+    const onBlur = event => {
+        commitAspect(event.target.value)
+        setInputFocus(false)
+    }
+
+    useEffect(() => {
+        if (isInputFocused) {
+            inputRef.current.select()
+        }
+    }, [aspects, isInputFocused, activeAspect.index])
+
+    const renderedValue = isInputFocused
+        ? aspects.map((a,i) => (
+            a.type === 'value'
+            ? (
+                <span
+                    key={i}
+                    className={clsx(
+                        style.value,
+                        activeAspect.index === i && style.valueFocus,
+                    )}
+                    onMouseDown={event => {
+                        updateAspect({type: 'setFocus', index: i})
+                        event.preventDefault()
+                    }}
+                    onClick={event => event.stopPropagation()}
+                >
+                    { i === activeAspect.index ? activeAspect.value : a.value }
+                </span>
+            )
+            : (
+                <span
+                    key={i}
+                    className={style.formatter}
+                >
+                    {a.placeholder}
+                </span>
+            )
+        ))
+        : display
+            ? <span className={style.value}>{display}</span>
+            : <span className='placeholder'>{placeholder}</span>
+
+    return (
+        <div
+            tabIndex={-1}
+            onFocus={onFocusProp}
+            onBlur={onBlurProp}
+            className={clsx (
+                className,
+                style.input
+            )}
+        >
+            { renderedValue }
+            <input
+                ref={forkedInputRef}
+                type='number'
+                onBlur={onBlur}
+                onFocus={onFocus}
+                onKeyDown={onKeyDown}
+                onChange={onChange}
+                value={isInputFocused && activeAspect.value}
+                style={{maxHeight: 0, maxWidth: 0, padding: 0, margin: 0, border: 0}}
+            />
+        </div>
+    )
+})
+
+export default AspectInput
--- src/DateTime/DateTimeInput.js
+++ src/DateTime/DateTimeInput.js
@@ -1,20 +1,6 @@
-import React, { useCallback, useContext, useEffect, useMemo, useReducer, useRef, useState } from 'react'
+import React, { useCallback, useMemo } from 'react'
 
-import MuiPickersAdapterProvider, { MuiPickersAdapterContext } from '@material-ui/pickers/LocalizationProvider'
-import DateFns from '@material-ui/pickers/adapter/date-fns'
-import { useForkedRef } from '../util/ref'
-import { makeStyles } from '@material-ui/core'
-import clsx from 'clsx'
-import { shallowEqual } from '../util/equal'
-
-const useStyle = makeStyles(theme => ({
-    value: {
-    },
-    valueFocus: {
-        background: theme.palette.type === 'light' ? theme.palette.primary.light : theme.palette.primary.dark,
-        color: theme.palette.primary.contrastText,
-    },
-}))
+import { AspectInput } from '../AspectInput/AspectInput'
 
 let daysInMonth = (dateUtil, ...args) => {
     const d = typeof(args[0]) === 'object'
@@ -24,228 +10,78 @@ let daysInMonth = (dateUtil, ...args) => {
     return Number(dateUtil.formatByString(end, 'dd'))
 }
 
-const validateValue = (dateUtil, value, placeholder, currentDate) => {
-    value = Number(value)
+const validateAspect = (dateUtil, value, aspectValue, aspectIndex) => {
+    const placeholder = value.input[aspectIndex].placeholder
+
+    aspectValue = Number(aspectValue)
+
     if (placeholder === 'yyyy') {
-        // might want to use min and max date for autocorrection of small values
-        return value < 100
-            ? value + (value > 30 ? 1900 : 2000)
-            : value <= 2100 ? value : undefined
+        // might want to use min and max date for autocorrection of small aspectValues
+        return aspectValue < 100
+            ? aspectValue + (aspectValue > 30 ? 1900 : 2000)
+            : aspectValue <= 2100 ? aspectValue : undefined
     } else if (placeholder === 'MM') {
-        return value >= 1 && value <= 12 ? value : undefined
+        return aspectValue >= 1 && aspectValue <= 12 ? aspectValue : undefined
     } else if (placeholder === 'dd') {
-        return value >= 1 && value <= daysInMonth(dateUtil, currentDate) ? value : undefined
+        return aspectValue >= 1 && aspectValue <= daysInMonth(dateUtil, value.parsed) ? aspectValue : undefined
     } else if (placeholder === 'HH') {
-        return value >= 0 && value <= 23 ? value : undefined
+        return aspectValue >= 0 && aspectValue <= 23 ? aspectValue : undefined
     } else if (placeholder === 'mm' || placeholder === 'ss') {
-        return value >= 0 && value <= 59 ? value : undefined
+        return aspectValue >= 0 && aspectValue <= 59 ? aspectValue : undefined
     }
 }
 
-const resetInput = (value) => {
-    const i = value.input.findIndex(p => p.type === 'value')
-    const v = value.input[i].value
-    return {index: i, value: v}
+const commitAspect = (dateUtil, value, onChange, aspectValue, aspectIndex) => {
+    const newDate = dateUtil.parse(
+        value.input.map((p,i) => {
+            if (p.type === 'formatter') {
+                return p.placeholder
+            }
+            if (i === aspectIndex) {
+                return aspectValue
+            }
+
+            // when switching to a month with less days, date might need correction
+            if (p.placeholder === 'dd'
+                && value.input[aspectIndex].placeholder === 'MM'
+            ) {
+                const y = value.input.find(p => p.placeholder[0] === 'y')
+                return Math.min(daysInMonth(dateUtil, y && y.value, aspectValue), p.value)
+            }
+
+            return p.value
+        }).join(''),
+        value.input.map(p => p.placeholder).join(''),
+    )
+
+    if (String(newDate) !== 'Invalid Date') {
+        onChange(newDate)
+    }
 }
 
 export const DateTimeInput = React.forwardRef(function DateTimeInput(props, ref) {
     const {
-        className,
-        classes,
-
-        onBlur: onBlurProp,
-        onFocus: onFocusProp,
-        
         dateUtil,
-        onChange: onChangeProp,
+        onChange,
         value,
         placeholder,
 
         ...others
     } = props
 
-    const style = useStyle()
+    const validate = useMemo(() => validateAspect.bind(undefined, dateUtil, value), [dateUtil, value])
 
-    const inputRef = useRef()
-    const forkedInputRef = useForkedRef(ref, inputRef)
-
-    const [input, updateInput] = useReducer((state, action) => {
-        if (action.type === 'moveFocus' && action.step) {
-            for (let i = state.index + action.step; i >= 0 && i < value.input.length; i += action.step) {
-                if (value.input[i].type === 'value') {
-                    return {index: i, value: value.input[i].value}
-                }
-            }
-        }
-        if (action.type === 'setFocus' && action.index !== undefined) {
-            return {index: action.index, value: value.input[action.index].value}
-        }
-        if (action.type === 'reset') {
-            return resetInput(value)
-        }
-        if (action.type === 'change') {
-            return {index: state.index, value: action.value}
-        }
-        return state
-    }, value, resetInput)
-
-    useEffect(() => {
-        if (isInputFocused && value.input[input.index].value,input.value) {
-            updateInput({type: 'change', value: value.input[input.index].value})
-        } else if (!isInputFocused && !shallowEqual(resetInput(value), input)) {
-            updateInput({type: 'reset'})
-        }
-    }, [value])
-
-    const [isInputFocused, setInputFocus] = useState(false)
-
-    const onFocus = useCallback(event => setInputFocus(true), [setInputFocus])
-
-    const onChange = event => {
-        let newValue = event.target.value === ''
-            || validateValue(dateUtil, event.target.value, value.input[input.index].placeholder, value.parsed)
-            ? event.target.value
-            : validateValue(dateUtil, event.target.value.substr(-1), value.input[input.index].placeholder, value.parsed)
-                && event.target.value.substr(-1)
-        if (newValue === undefined) {
-            return
-        }
-        newValue = String(Number(newValue)).padStart(value.input[input.index].placeholder.length, '0')
-
-        if (newValue > 0 && validateValue(dateUtil, newValue + '0', value.input[input.index].placeholder, value.parsed) === undefined) {
-            updateInput({type: 'change', value: newValue})
-            updateInput({type: 'moveFocus', step: 1})
-            commitValue(newValue)
-        } else {
-            updateInput({type: 'change', value: newValue})
-        }
-    }
-    const commitValue = committedValue => {
-        const newValue = validateValue(dateUtil, committedValue, value.input[input.index].placeholder, value.parsed)
-        if (newValue === undefined) {
-            return
-        }
-
-        const newDate = dateUtil.parse(
-            value.input.map((p,i) => {
-                if (p.type === 'formatter') {
-                    return p.placeholder
-                }
-                if (i === input.index) {
-                    return newValue
-                }
-
-                // when switching to a month with less days, date might need correction
-                if (p.placeholder === 'dd'
-                    && value.input[input.index].placeholder === 'MM'
-                ) {
-                    const y = value.input.find(p => p.placeholder[0] === 'y')
-                    return Math.min(daysInMonth(dateUtil, y && y.value, newValue), p.value)
-                }
-
-                return p.value
-            }).join(''),
-            value.input.map(p => p.placeholder).join(''),
-        )
-
-        if (String(newDate) !== 'Invalid Date') {
-            onChangeProp(newDate)
-        }
-    }
-    const onBlur = event => {
-        commitValue(event.target.value)
-        setInputFocus(false)
-    }
-    const onKeyDown = event => {
-        const moveKeys = {
-            'ArrowLeft': -1,
-            'ArrowRight': 1,
-        }
-        if (moveKeys[event.key]) {
-            event.preventDefault()
-
-            commitValue(event.target.value)
-            updateInput({type: 'moveFocus', step: moveKeys[event.key]})
-        }
-
-        const addKeys = {
-            'ArrowDown': -1,
-            'ArrowUp': 1,
-        }
-        if (addKeys[event.key]) {
-            event.preventDefault()
-
-            let newValue = Number(event.target.value) + addKeys[event.key]
-            newValue = validateValue(dateUtil, newValue, value.input[input.index].placeholder, value.parsed)
-            if (newValue === undefined) {
-                return
-            }
-            newValue = String(Number(newValue)).padStart(value.input[input.index].placeholder.length, '0')
-
-            updateInput({type: 'change', value: newValue})
-        }
-    }
-
-    useEffect(() => {
-        if (isInputFocused) {
-            inputRef.current.select()
-        }
-    }, [value, isInputFocused, input.index])
-
-    const renderedValue = isInputFocused
-        ? value.input.map((part,i) => (
-            part.type === 'value'
-            ? (
-                <span
-                    key={i}
-                    className={clsx(
-                        style.value,
-                        input.index === i && style.valueFocus,
-                    )}
-                    onMouseDown={event => {
-                        updateInput({type: 'setFocus', index: i})
-                        event.preventDefault()
-                    }}
-                    onClick={event => event.stopPropagation()}
-                >
-                    { i === input.index ? input.value : part.value }
-                </span>
-            )
-            : (
-                <span
-                    key={i}
-                    className={style.formatter}
-                >
-                    {part.placeholder}
-                </span>
-            )
-        ))
-        : value.display
-            ? <span className={style.value}>{value.display}</span>
-            : <span className='placeholder'>{placeholder}</span>
+    const commit = useMemo(() => commitAspect.bind(undefined, dateUtil, value, onChange), [dateUtil, value, onChange])
 
     return (
-        <div
-            tabIndex={-1}
-            onFocus={onFocusProp}
-            onBlur={onBlurProp}
-            className={clsx (
-                className,
-                style.input
-            )}
-        >
-            { renderedValue }
-            <input
-                ref={forkedInputRef}
-                type='number'
-                onBlur={onBlur}
-                onFocus={onFocus}
-                onKeyDown={onKeyDown}
-                onChange={onChange}
-                value={isInputFocused && input.value}
-                style={{maxHeight: 0, maxWidth: 0, padding: 0, margin: 0, border: 0}}
-            />
-        </div>
+        <AspectInput
+            {...others}
+            validate={validate}
+            commit={commit}
+            aspects={value.input}
+            display={value.display}
+            placeholder={placeholder}
+        />
     )
 })
 
