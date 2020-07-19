866cfaa943ee1059aeae2b32f20ff2772531d514
Philipp Fritsche <ph.fritsche@gmail.com>
2020-07-19 15:43:29 +0200

    Bump deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,28 @@
-const conf = require('liform-theme-dev/webpack.config')
+var Encore = require('@symfony/webpack-encore')
+
+Encore.configureRuntimeEnvironment('dev')
+
+const conf = Encore
+    .setOutputPath('build/')
+    .setPublicPath('/build')
+
+    .addEntry('liform', process.cwd() + '/liform.jsx')
+
+    .enableReactPreset()
+
+    .disableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .enableIntegrityHashes(Encore.isProduction())
+
+.getWebpackConfig()
+
+conf.devServer = {
+    host: '0.0.0.0',
+    port: 8000,
+    historyApiFallback: true,
+}
 
 module.exports = conf
