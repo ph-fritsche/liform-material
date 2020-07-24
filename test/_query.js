e446e364e5b50df330f79d1335c8403628b2c6e0
Philipp Fritsche <ph.fritsche@gmail.com>
2020-07-24 10:01:47 +0200

    Fix query

--- test/_query.js
+++ test/_query.js
@@ -2,10 +2,11 @@ import { buildQueries, queryAllByLabelText, queryAllByText } from '@testing-libr
 
 export const [queryByL, getAllByL, getbyL, findAllByL, findByL] = buildQueries(
     (container, text, options) => {
-        const label = queryAllByLabelText(container, text, options)
+        const input = queryAllByLabelText(container, text, options)
         const legend = queryAllByText(container, text, {...options, selector: 'legend'})
+        const fieldset = legend.map(e => e.closest('fieldset')).filter(e => e !== null)
 
-        return label || legend.length && legend.map(e => e.closest('fieldset')).filter(e => e !== null)
+        return input.concat(fieldset)
     },
     (container, text) => `Found multiple elements for label/legend "${text}"`,
     (container, text) => `Unable to find an input for label or fieldset for legend "${text}"`,
