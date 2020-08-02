49f3dc2d4fe35efce002c3de6eeb0890367c7d4e
Philipp Fritsche <ph.fritsche@gmail.com>
2020-08-02 19:43:24 +0200

    Rewrite ButtonWidget

--- src/Field/ButtonWidget.js
+++ src/Field/ButtonWidget.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import { Button } from '@material-ui/core'
-import { SchemaProp } from 'liform-react-final/dist/schema'
+import { LiformContextProp, SchemaProp } from 'liform-react-final'
 
 export const ButtonWidget = props => {
     const {
         schema = true,
     } = props
 
+    const types = ['submit', 'reset']
+    let type = undefined
+    if (typeof(schema.widget) === 'string') {
+        type = types[types.indexOf(schema.widget)]
+    } else if (Array.isArray(schema.widget)) {
+        type = types.filter(t => schema.widget.indexOf(t) >= 0)[0]
+    }
+
     return (
-        <Button variant="contained" color="primary">
+        <Button
+            variant={ types.indexOf(type) >= 0 && 'contained' || 'text' }
+            color={ type === 'submit' && 'primary' || 'default' }
+            type={type}
+        >
             {schema.title}
         </Button>
     )
