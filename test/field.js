c5fba5d9f411d38ae15461f98b832fd012bb0dac
Philipp Fritsche <ph.fritsche@gmail.com>
2020-08-02 18:51:33 +0200

    Refactor

--- test/field.js
+++ test/field.js
@@ -1,6 +1,65 @@
-import { testLifield } from './_field'
+import React from 'react'
+import { render, queries } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { fireEvent } from '@testing-library/react'
+import { Liform, Lifield, htmlizeName } from 'liform-react-final'
+import * as customQueries from './_query'
+import MaterialTheme from '../src'
+
+function FieldTestLiform (props) {
+    return (
+        <Liform
+            theme={MaterialTheme}
+            name="form"
+            render={{container: p => <form id="container" data-values={JSON.stringify(p.form.getState().values._)}>{p.children}</form>}}
+            {...props}
+        >
+            { ({liform}) => <Lifield schema={liform.schema}/> }
+        </Liform>
+    )
+}
+
+function renderLifield (props) {
+    const result = render(FieldTestLiform(props), {queries: {...queries, ...customQueries}})
+    return {
+        result,
+        form: result.container.querySelector('form#container'),
+        expectedFormValues: expectedFormValues(props.value, props.name || 'form'),
+    }
+}
+
+function expectedFormValues(value, rootName) {
+    const expected = {}
+
+    function traverseValue(value, path = ['_']) {
+        if (typeof(value) === 'object') {
+            if (Array.isArray(value)) {
+                value.forEach((v,i) => traverseValue(v, [].concat(path, [i])))
+            } else {
+                for (const k in value) {
+                    traverseValue(value[k], [].concat(path, k))
+                }
+            }
+        } else {
+            expected[htmlizeName(path.join('.'), rootName)] = value
+        }
+    }
+    value !== undefined && traverseValue(value)
+
+    return expected
+}
+
+export function testLifield (props) {
+    const rendered = renderLifield(props)
+
+    expect(rendered.form).toContainFormValues(rendered.expectedFormValues)
+
+    if (props.schema && props.schema.title) {
+        rendered.field = rendered.result.getbyL(props.schema.title)
+    }
+
+    return rendered
+}
 
 describe('Basic input', () => {
     it('Render and change string input', () => {
