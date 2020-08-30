4ae19ed59f3c7005590708390a1d886b7c7f9e80
Philipp Fritsche <ph.fritsche@gmail.com>
2020-08-30 13:31:32 +0200

    Rewrite DateTime field

--- src/Field/DateTime.js
+++ src/Field/DateTime.js
@@ -4,37 +4,32 @@ import { DateTimeField } from '../Component/DateTime/DateTimeField'
 import { BaseRender } from './BaseRender'
 import { FieldRenderProps } from 'liform-react-final'
 
-const valueFormatFromSchema = (schema) => {
-    if (schema.dateFormat) {
-        return schema.dateFormat
-    }
+const jsonFormats = {
+    'datetime-local': "yyyy-MM-dd'T'HH:mm:sszzz",
+    'date': 'yyyy-MM-dd',
+    'time': 'HH:mm:sszzz',
+}
 
-    if (schema.format === 'datetime-local') {
-        return "yyyy-MM-dd'T'HH:mm:sszzz"
-    } else if (schema.format === 'date') {
-        return 'yyyy-MM-dd'
-    } else if (schema.format === 'time') {
-        return 'HH:mm:sszzz'
-    }
+const widgetFormats = {
+    'datetime': "yyyy-MM-dd'T'HH:mm",
+    'date': 'yyyy-MM-dd',
+    'time': 'HH:mm',
 
-    const widget = Array.isArray(schema.widget) ? schema.widget : [schema.widget]
-    if (widget.indexOf('datetime') >= 0) {
-        return "yyyy-MM-dd'T'HH:mm"
-    } else if (widget.indexOf('date') >= 0) {
-        return 'yyyy-MM-dd'
-    } else if (widget.indexOf('time') >= 0) {
-        return 'HH:mm'
-    } else if (widget.indexOf('week') >= 0) {
-        // https://github.com/dmtrKovalenko/date-io/issues/412
-        // return "YYYY-'W'ww"
-        return "RRRR-'W'II"
-    }
+    // https://github.com/dmtrKovalenko/date-io/issues/412
+    // "YYYY-'W'ww"
+    'week': "RRRR-'W'II",
 }
 
 export const DateTime = props => {
     const {
         schema = true,
-        valueFormat = valueFormatFromSchema(schema),
+        valueFormat = schema.dateFormat
+            || schema.format && jsonFormats[schema.format]
+            || widgetFormats[Object.keys(widgetFormats).find(k =>
+                Array.isArray(schema.widget)
+                    ? schema.widget.includes(k)
+                    : schema.widget === k
+            )],
     } = props
 
     return (
