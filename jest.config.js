4f0c5cdcd65fb98a9efc4a5e38fefd6919e684ba
Philipp Fritsche <ph.fritsche@gmail.com>
2021-08-03 11:51:45 +0000

    chore: replace babel with ts-jest

--- jest.config.js
+++ jest.config.js
@@ -2,19 +2,26 @@ module.exports = {
     verbose: true,
     collectCoverage: true,
     collectCoverageFrom: [
-        'src/**/*.{js,jsx}',
+        'src/**/*.{js,jsx,ts,tsx}',
     ],
     coveragePathIgnorePatterns: [],
     testMatch: [
-        '<rootDir>/test/**/*.{js,jsx}',
+        '<rootDir>/test/**/*.{js,jsx,ts,tsx}',
     ],
     testPathIgnorePatterns: [
         '/_.*(?<!.test.js)$',
     ],
+    transform: {
+        '\\.([tj]sx?)$': 'ts-jest',
+    },
     transformIgnorePatterns: [
+        '/node_modules/',
     ],
     setupFilesAfterEnv: [
         '<rootDir>/test/_setup.js',
     ],
     testEnvironment: 'jsdom',
+    moduleNameMapper: {
+        '@material-ui/core/(.*)': '@material-ui/core/node/$1',
+    },
 }
